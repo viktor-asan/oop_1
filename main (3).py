2a56dc670a1ae02e19022b72ca9dae4018c695a1
viktor-asan <viktordon225@gmail.com>
2024-09-18 11:03:36 +0500

    наследование
    добавил новый класс

---
+++ main (3).py
@@ -0,0 +1,35 @@
+# Наследование - классы могут наследовать атрибуты и методы
+
+# класс пользователь 
+class user:
+    def __init__(self, fullname, login password , email):
+        self._fullname = fullname 
+        self._login = login
+        self._password = password 
+        self._email = email 
+    # геттеры
+    @property
+    def fullname (self):
+        restun self._fullname
+    @property
+    def login(self):
+        restun self._login
+    @property 
+    def password(self):
+        def password
+        resturn self._password
+    def email(self):
+        def email(self): 
+        return self._email 
+    # сеттеры 
+    @fullname.setter
+
+
+
+
+
+
+#добавить класс
+class NewRoot:
+    def __init__(self,params):
+        self.params = params
