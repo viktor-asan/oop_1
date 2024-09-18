8a1248bd2963ea5c21662040e23890235fbe7dd2
viktor-asan <viktordon225@gmail.com>
2024-09-18 10:44:00 +0500

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,26 @@
+# Наследование - классы могут наследовать атрибуты и методы
+
+# класс пользователь 
+class user:
+    def __init__(self, fullname, login password , email):
+        self._fullname = fullname 
+        self._login = login
+        self._password = password 
+        self._email = email 
+    # геттеры
+    @property
+    def fullname (self):
+        restun self._fullname
+    @property
+    def login(self):
+        restun self._login
+    @property 
+    def password(self):
+        def password
+        resturn self._password
+    def email(self):
+        def email(self): 
+        return self._email 
+    # сеттеры 
+    @fullname.setter
+    
