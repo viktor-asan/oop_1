ed550f7ffd21d363fe4ddf883e4bc64505cd9802
viktor-asan <viktordon225@gmail.com>
2025-02-03 15:28:02 +0500

    Update main (3).py

--- main (3).py
+++ main (3).py
@@ -1,35 +1,33 @@
-# Наследование - классы могут наследовать атрибуты и методы
+from flask import Flask, render_template, request,redirect 
 
-# класс пользователь 
-class user:
-    def __init__(self, fullname, login password , email):
-        self._fullname = fullname 
-        self._login = login
-        self._password = password 
-        self._email = email 
-    # геттеры
-    @property
-    def fullname (self):
-        restun self._fullname
-    @property
-    def login(self):
-        restun self._login
-    @property 
-    def password(self):
-        def password
-        resturn self._password
-    def email(self):
-        def email(self): 
-        return self._email 
-    # сеттеры 
-    @fullname.setter
+#создать объект класса Flask
+application = Flask(__name__)
+
+@application.route('/',methods = ['POST','GET'])
+def home():
+ title = "Вход"
+ message =''
+ login = request.form.get('login')
+ password = request.form.get('password')
+
+ if request.method =='POST':
+     if UserController.auth(login,password):
+         return redict('/admin')
+     else:
+         message= 'Не верный логин или пароль'
 
 
+ return render_template('login.html',title = title,
+                        message = message)
 
 
+@application.route('/test')
+def test():
+   return "<h1>это тест</h1>"
+@application.route('/param/<id>')
+def admin():
+    title='Панель Админа'
 
-
-#добавить класс
-class NewRoot:
-    def __init__(self,params):
-        self.params = params
+    return render_template ('admin.html',title=title)
+if __name__=="__main__":
+   application.run(debug=True, host = '0.0.0.0')
